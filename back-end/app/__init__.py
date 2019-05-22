c86dcfe384d7c4febf5d8630fd2786cf469c40d2
gotowit <gotowit@163.com>
2019-05-22 10:28:16 +0800

    2...

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from flask_cors import CORS
 from config import Config
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    CORS(app)
 
     # 注册 blueprint
     from app.api import bp as api_bp
